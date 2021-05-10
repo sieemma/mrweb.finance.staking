48f8de1be08333b07e61db997bda18cabb6f4d9e
sieemma <35629714+sieemma@users.noreply.github.com>
2021-05-10 02:07:13 +0000

    Update Utils.js

--- src/utils/Utils.js
+++ src/utils/Utils.js
@@ -28,7 +28,7 @@ export const initContract = async () => {
     return null;
   }
 
-  const contractAddress = `TSLfeJAHSzo2VzN3MtAKy8b8Aw754MKVja`;
+  const contractAddress = `TWWZLY8Aa444ZYCNQMCmMT8g1iQNhtqpUx`;
   let contract = await window.tronWeb.contract().at(contractAddress);
   return contract;
 };
